77a4fdc24fec022ae524954f4b77c14be3b77a5f
hechengjin <hechengjin@kingsoft.com>
2017-08-24 19:42:34 +0800

    增加window测试版本

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mySite
 本地服务发布到公网
+
+node启动
+先修改 server.js 中的 documentRoot 变量的值 
+node server.js
+
+
+ngrok启动
+>ngrok http localhost:8888                  # request subdomain name: 'xxx.ngrok.io'    xxx为随机数  如： http://7c03161f.ngrok.io
+
+ngrok http -subdomain=mysite localhost:8888     # request subdomain name: 'bar.ngrok.io'   要注册账号才可以
---
+++ windows/server.js
@@ -0,0 +1,50 @@
+﻿var http = require('http');
+var fs = require('fs');
+
+var documentRoot = 'C:/gitProjects/mySite/windows/www';
+
+var clientSet = new Set();
+var lastCount = 0;
+
+var server= http.createServer(function(req,res){
+
+    var url = req.url; 
+    //客户端输入的url，例如如果输入localhost:8888/index.html
+    //那么这里的url == /index.html 
+	if(url == '/'){
+		url = '/index.html';
+	}
+	
+	var ipAddress;
+	var headers = req.headers;
+	var forwardedIpsStr = headers['x-real-ip'] || headers['x-forwarded-for'];
+	forwardedIpsStr ? ipAddress = forwardedIpsStr : ipAddress = null;
+	if (!ipAddress) {
+	ipAddress = req.connection.remoteAddress;
+	}
+	clientSet.add(ipAddress);
+	if(lastCount != clientSet.size){
+		lastCount = clientSet.size;
+		console.log("  " + lastCount)	
+	}
+	
+	
+
+    var file = documentRoot + url;
+
+    fs.readFile( file , function(err,data){
+        if(err){
+            res.writeHeader(404,{
+                'content-type' : 'text/html;charset="utf-8"'
+            });
+            res.write('<h1>404错误</h1><p>你要找的页面不存在</p>');
+            res.end();
+        }else{
+            res.write(data);//将index.html显示在客户端
+            res.end();
+        }
+    });
+
+}).listen(8888);
+
+console.log('服务器开启成功');
